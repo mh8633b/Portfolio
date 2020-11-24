f07d2fb672f7ed98105fde9d8f41bd83dd8421ee
monybluex <35287609+monybluex@users.noreply.github.com>
2020-11-24 20:55:06 +0500

    lounch1

--- muneebulhassan/views.py
+++ muneebulhassan/views.py
@@ -3,29 +3,32 @@ from django.core.mail import send_mail
 from django.shortcuts import render
 import requests
 import itertools
-
-
-# Create your views here.
 from muneebulhassan.models import project, experience, experiencedetail, connect
 
 
 def index(request):
-    # if(request.method=="POST"):
-    #     name = request.POST.get('name','')
-    #     subject = request.POST.get('subject','')
-    #     email = request.POST.get('email','')
-    #     cnt = connect(u_name=name, u_subject=subject , u_email=email)
-    #     cnt.save()
-    #     send_mail("abc","asdasdad", 'mh8633b@gmail.com',['monybluex@gmail.com'])
-    #
+    if(request.method=="POST"):
+        print("fom btn clicked")
+        name = request.POST.get('name','')
+        subject = request.POST.get('subject','')
+        email = request.POST.get('email','')
+        cnt = connect(u_name=name, u_subject=subject , u_email=email)
+        cnt.save()
+        send_mail("abc","asdasdad", 'mh8633b@gmail.com',['monybluex@gmail.com'])
 
-    # repo_list_url =
     repo = requests.get("https://api.github.com/users/monybluex/repos")
     repo_data = repo.json()
-    projects = project.objects.all
+    # print(repo_data)
+    for data in repo_data:
+        lang_url = data['languages_url']
+        languages = requests.get(lang_url)
+        lang_data = languages.json()
+        out = dict(itertools.islice(lang_data.items(), 3))
+        data['lang'] = out
+
+    # projects = project.objects.all
     exp = experience.objects.all
     expd = experiencedetail.objects.all()
-    for data in repo_data:
-        print(data['name'])
-    parms ={'project' : projects, 'experience': exp, 'exp_detail' : expd, 'repos' : repo_data }
+
+    parms ={'experience': exp, 'exp_detail' : expd, 'repos' : repo_data }
     return render(request, "index.html",parms)
